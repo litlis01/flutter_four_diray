dc2803436e2fe7d131366252578fd1b6ed9706e5
litlis01 <litlis@naver.com>
2025-09-16 22:58:07 +0900

    Add initial UI structure for the main diary view

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'dart:math' as math;
+
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,16 +14,109 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: '네컷일기',
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: Scaffold(
-        body: Center(
-          child: Text("hello world!"),
+      theme: ThemeData(useMaterial3: false),
+      home: MainScreen(),
+    );
+  }
+}
+
+class MainScreen extends StatefulWidget {
+  const MainScreen({super.key});
+
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          '네컷일기',
+          style: GoogleFonts.nanumPenScript(
+            textStyle: TextStyle(
+              fontSize: 16,
+              color: Colors.black,
+              fontWeight: FontWeight.w800,
+            ),
+          ),
         ),
-      )
+        backgroundColor: Colors.white,
+        elevation: 0,
+      ),
+      body: Container(
+        color: Color(0xffFAF9E6),
+        child: Stack(
+          alignment: Alignment.center,
+          children: [
+            Container(
+              width: double.maxFinite,
+              height: MediaQuery.of(context).size.width,
+              child: GridView(
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  crossAxisSpacing: 8,
+                  mainAxisSpacing: 8,
+                ),
+                physics: const NeverScrollableScrollPhysics(),
+                children: [
+                  Container(color: Colors.black),
+                  Container(color: Colors.white),
+                  Container(color: Colors.yellow),
+                  Container(color: Colors.blue),
+                ],
+              ),
+            ),
+            Transform.rotate(
+              angle: 60 * math.pi / 180,
+              child: Container(
+                width: MediaQuery.of(context).size.width / 4,
+                height: 120,
+                decoration: BoxDecoration(
+                  color: Colors.black.withOpacity(0.5),
+                  borderRadius: BorderRadius.all(Radius.elliptical(70, 85)),
+                ),
+              ),
+            ),
+            Text(
+              '제목입니다.',
+              style: GoogleFonts.nanumPenScript(
+                textStyle: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.w800,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+            Positioned(
+              bottom: 0,
+              right: 0,
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.black.withOpacity(0.5),
+                  borderRadius: BorderRadius.circular(4),
+                ),
+                child: Container(
+                  margin: EdgeInsets.all(8),
+                  child: Text(
+                    '2025.09.16',
+                    style: GoogleFonts.nanumPenScript(
+                      textStyle: TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.w500,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
